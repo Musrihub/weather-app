a620eecba2d939f02762ab54445c5396367b7caa
Lionel NTIRUSHWA <linog02@gmail.com>
2023-08-01 10:53:23 +0200

    added API key

---
+++ script.js
@@ -0,0 +1,21 @@
+const apiKey = "3b451342cc0605de8addc7069e1650ae";
+const apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+
+async function checkWeather(city) {
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+  let data = await response.json();
+
+  document.querySelector(".city").innerHTML = data.name;
+  document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+  document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+  document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+  console.log(data);
+}
+// checkWeather();
+searchBtn.addEventListener("click", () => {
+   checkWeather(searchBox.value);
+ });
